ded979c12c22c22d169bf35f99d74a4dc300599b
Manuvikash <manuvikashs@gmail.com>
2021-10-10 16:23:53 +0530

    change

--- portfolio-app/src/components/calendar/Calendar.jsx
+++ portfolio-app/src/components/calendar/Calendar.jsx
@@ -1,17 +1,5 @@
 import "./calendar.scss";
-// import { init } from "ityped";
-// import { useEffect, useRef } from "react";
 
 export default function Calendar() {
-  // const textRef = useRef();
-  // useEffect(() => {
-  //   init(textRef.current, {
-  //     showCursor: true,
-  //     strings: ["Developer", "Engineer", "Fighter pilot"],
-  //     typeSpeed: 150,
-  //     backSpeed: 75,
-  //     backDelay: 1000,
-  //   });
-  // }, []);
   return <div className="calendar" id="calendar"></div>;
 }
